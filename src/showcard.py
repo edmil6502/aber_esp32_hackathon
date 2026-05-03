20f43299d2a16d31cfe550e79c1a3b7ab8037838
Eddie Millichip <edmil@crispin.local>
2026-05-03 17:00:30 +0100

    final commit

--- src/showcard.py
+++ src/showcard.py
@@ -2,22 +2,40 @@
 
 	showcard.py
 
-	
+	Shows a card to the user. The user should see the question first, then tap the screen to reveal the answer. 
 
 '''
 
 import init
 import lvgl as lv
 
+import cardmanager
+
+import random
+import time
+
 scr = lv.screen_active()
 
-flashcards = [["where is pee stored", "the balls"], ["what is the best animal?", "horse"]]
-card = flashcards[0]
 
 questionText = lv.label(scr)
-questionText.set_text(card[0])
-questionText.set_pos(60, 50)
-
 answerText = lv.label(scr)
-answerText.set_text(card[1])
+
+questionText.set_pos(60, 50)
 answerText.set_pos(60, 100)
+
+
+button = lv.button(scr)
+button.center()
+
+# update what is on screen to match the contents of the card
+while True:
+	card = cardmanager.getCard()
+
+	questionText.set_text(card[0])
+	answerText.set_text(card[1])
+	time.sleep(5)
+
+def removeCard():
+	print("greer")
+	
+button.add_event_cv(removeCard, lvgl.EVENT.ALL, None)
